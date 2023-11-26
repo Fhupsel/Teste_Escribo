816a74b00c409dbee00ccf49560805d60a97c804
Felipe Monteiro Hupsel <fmhupsel@outlook.com>
2023-11-26 12:22:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-console.log('Insira um número!');
+///Script para caso seja testado via console
+function DesafioConsole(numero){
+  const listaN = []
+  for(let i = 0; i<numero; i++){
+    listaN.push(i);
+  }
+  const listaFiltro = listaN.filter(n => n%3 == 0 || n%5==0);
+  const total = listaFiltro.reduce((acumulador, atual) => {
+    return acumulador + atual
+  }, 0);
+  console.log(total);
+}
 
+////Script para caso seja testado via web
 function desafio(){
   const res = document.querySelector('h2');
   const lista = [];
   const value = document.getElementById('num').value;
   const num = Number(value);
-  console.log('Esse foi o número inserido: ', num);
   for(let n = 0; n<num; n++){
     lista.push(n);
   }
-  console.log(lista);
   const listaFiltrada = lista.filter(n => n%3 == 0 || n%5==0);
-  console.log(listaFiltrada);
   const soma = listaFiltrada.reduce((acumulador, atual) => {
     return acumulador + atual
   }, 0);
-  console.log('O valor do somatório é: ', soma);
   res.innerText = 'O valor do somatório é: ' + soma;
 }
